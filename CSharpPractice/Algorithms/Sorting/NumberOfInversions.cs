f6e9b6d32cfa77c282fa01b805c606f84b6e9acf
Dekkeret <kdushin1990@gmail.com>
2018-04-02 08:54:57 +0300

    CountInversions

---
+++ CSharpPractice/Algorithms/Sorting/NumberOfInversions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpPractice.Algorithms.Sorting
+{
+    public class NumberOfInversions
+    {
+        //{1, 5, 2, 6}
+        public static int SortAndCountInversions(int[] a, int left, int right)
+        {
+            int numberOfInversions = 0;
+            var tempArr = new int[a.Length];
+            if (right > left)
+            {
+                int middle = (left + right) / 2;
+
+                // Divide and merge into sub arrays
+                numberOfInversions += SortAndCountInversions(a, left, middle);
+                numberOfInversions += SortAndCountInversions(a, middle + 1, right);
+                numberOfInversions += MergeAndCountSplitInversion(a, tempArr, left, middle + 1, right);
+            }
+
+            return numberOfInversions;
+        }
+
+        private static int MergeAndCountSplitInversion(int[] arr, int[] temp, int left, int middle, int right)
+        {
+            
+        }
+    }
+}
--- Tests/SortingTests.cs
+++ Tests/SortingTests.cs
@@ -25,11 +25,13 @@ namespace Tests
             Assert.That(unsortedArray, Is.EqualTo(sortedArray));
         }
 
+        
+
         [Test]
         public void TestMergeSortOdd()
         {
-            var unsortedArray = new int[] { 1, 3, 2, 11, 4, 5, 8, 6, 7};
-            var sortedArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 11 };
+            var unsortedArray = new int[] {1, 3, 2, 11, 4, 5, 8, 6, 7};
+            var sortedArray = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 11};
             Merge.Sort(unsortedArray);
             Assert.That(unsortedArray, Is.EqualTo(sortedArray));
         }
@@ -37,8 +39,8 @@ namespace Tests
         [Test]
         public void TestMergeSortRepeatedNumbers()
         {
-            var unsortedArray = new int[] { 1, 3, 2, 4, 5, 8, 2, 6, 1, 7 };
-            var sortedArray = new int[] { 1, 1, 2, 2, 3, 4, 5, 6, 7, 8 };
+            var unsortedArray = new int[] {1, 3, 2, 4, 5, 8, 2, 6, 1, 7};
+            var sortedArray = new int[] {1, 1, 2, 2, 3, 4, 5, 6, 7, 8};
             Merge.Sort(unsortedArray);
             Assert.That(unsortedArray, Is.EqualTo(sortedArray));
         }
@@ -52,8 +54,8 @@ namespace Tests
         [Test]
         public void TestSelectionSort()
         {
-            var unsortedArray = new[] {22, 11, 99, 88, 9, 7, 42}; 
-            var sortedArray = new[] { 7, 9, 11, 22, 42, 88, 99 };
+            var unsortedArray = new[] {22, 11, 99, 88, 9, 7, 42};
+            var sortedArray = new[] {7, 9, 11, 22, 42, 88, 99};
             Selection.Sort(unsortedArray);
             Assert.That(unsortedArray, Is.EqualTo(sortedArray));
         }
@@ -69,11 +71,25 @@ namespace Tests
 
         [Test]
         public void TestSelectionSortSameNumbers()
-        {            
-            var actualArray = new[] { 22, 22, 3, 5, 66, 66, 90, 3, 5 };
-            var expectedArray = new[] { 3, 3, 5, 5, 22, 22, 66, 66, 90 };
+        {
+            var actualArray = new[] {22, 22, 3, 5, 66, 66, 90, 3, 5};
+            var expectedArray = new[] {3, 3, 5, 5, 22, 22, 66, 66, 90};
             Selection.Sort(actualArray);
             Assert.That(actualArray, Is.EqualTo(expectedArray));
+        }
+
+        [Test]
+        public void TestCountNumberOfInversions()
+        {
+            var arr = new int[] {3, 5, 2, 6, 4, 3};
+            NumberOfInversions.SortAndCountInversions(arr, 0, arr.Length);
+        }
+
+        [Test]
+        public void TestTemp()
+        {
+            var arr = new int[] { 3, 5, 2, 6, 4, 3 };
+            Merge.Sort(arr);
         }
     }
 }
